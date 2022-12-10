e280a56c27beec6d0f3bac53506378a057279615
David Diaz <davidadm16@gmail.com>
2022-12-10 12:51:32 -0500

    updating after not pushing code for a while

---
+++ FirstMVCApp/FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,37 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogvm = new DogViewModel();
+            return View(dogvm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dog)
+        {
+            dogs.Add(dog);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Hello()
+        {
+            DogViewModel dog = new DogViewModel()
+            { Name="Apollo", Age= 3};
+
+            return View(dog);
+        }
+    }
+}
---
+++ FirstMVCApp/FirstMVCApp/Controllers/HotelBookingController.cs
@@ -0,0 +1,16 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HotelBookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            var old = DateTime.Now.AddYears(-12);
+            var hb = new HotelBooking()
+            { Id= 1, GuestName= "David", DateStart= old, DateEnd= DateTime.Now};
+            return View(hb);
+        }
+    }
+}
