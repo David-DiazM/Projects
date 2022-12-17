004d026096c7c7c0672cb92bf860bee9ddd0dc73
David Diaz <davidadm16@gmail.com>
2022-12-16 19:24:56 -0500

    cleaned up contact

--- ContactWeb/ContactWebModels/Contact.cs
+++ ContactWeb/ContactWebModels/Contact.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.IO.Compression;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ContactWebModels
 {
