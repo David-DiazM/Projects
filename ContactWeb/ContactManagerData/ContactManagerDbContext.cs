880eff6265655ad7b183bbf1f4c2c8801e35cf8b
David Diaz <davidadm16@gmail.com>
2022-12-31 00:06:53 -0500

    added in the rework for contcts and tests for states and contacts

--- ContactWeb/ContactManagerData/ContactManagerDbContext.cs
+++ ContactWeb/ContactManagerData/ContactManagerDbContext.cs
@@ -24,12 +24,15 @@ namespace ContactManagerData
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var builder = new ConfigurationBuilder()
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
                             .SetBasePath(Directory.GetCurrentDirectory())
                             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-            _configuration = builder.Build();
-            var cnstr = _configuration.GetConnectionString("ContactManager");
-            optionsBuilder.UseSqlServer(cnstr);
+                _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("ContactManager");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
