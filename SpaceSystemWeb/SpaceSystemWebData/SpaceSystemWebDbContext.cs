0dbffa811d90ce260dc7652a895401235505537a
David Diaz <davidadm16@gmail.com>
2023-01-04 17:13:38 -0500

    updated database and created space system models

---
+++ SpaceSystemWeb/SpaceSystemWebData/SpaceSystemWebDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace SpaceSystemWebData
+{
+    public class SpaceSystemWebDbContext : DbContext
+    {
+        private static IConfigurationRoot _configuration;
+
+        public SpaceSystemWebDbContext()
+        {
+            //purposefully empty: Necessary for scaffolding
+        }
+
+        public SpaceSystemWebDbContext(DbContextOptions options)
+            : base(options)
+        {
+            //purposefully empty, sets options appropriately
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                _configuration = builder.Build();
+                var cnstr = _configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
+        }
+    }
+}
